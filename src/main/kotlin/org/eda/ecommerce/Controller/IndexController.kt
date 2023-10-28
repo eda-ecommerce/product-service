6a458c74ad532f7289874c7f9f79a2a6d9259d22
OliverWich <oliverwichmann@outlook.de>
2023-10-28 13:45:46 +0200

    feat: redirect / to swagger ui

---
+++ src/main/kotlin/org/eda/ecommerce/Controller/IndexController.kt
@@ -0,0 +1,21 @@
+package org.eda.ecommerce.Controller
+
+import jakarta.enterprise.context.ApplicationScoped
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.core.Response
+import jakarta.ws.rs.core.UriBuilder
+import org.eclipse.microprofile.openapi.annotations.Operation
+
+
+@ApplicationScoped
+@Path("/")
+class IndexController {
+
+    @GET
+    @Operation(summary = "Redirects to the Swagger UI")
+    fun redirectToSwagger(): Response {
+        val location = UriBuilder.fromPath("/q/swagger-ui").build()
+        return Response.seeOther(location).build()
+    }
+}
