7472eb689b6da84b483dbfe7a2bedfabac4f53d7
OliverWich <kontakt@oliver-wich.de>
2023-11-10 19:52:00 +0100

    test persistence and emitting on create

---
+++ src/test/kotlin/org/eda/ecommerce/GenericJSONSerdeFactory.kt
@@ -0,0 +1,15 @@
+package org.eda.ecommerce
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer
+import io.quarkus.kafka.client.serialization.ObjectMapperSerializer
+
+class GenericJSONSerdeFactory<T> {
+    fun createDeserializer(type : Class<T>) : ObjectMapperDeserializer<T>{
+        return ObjectMapperDeserializer(type)
+    }
+
+    fun createSerializer() : ObjectMapperSerializer<T> {
+        return ObjectMapperSerializer(ObjectMapper())
+    }
+}
